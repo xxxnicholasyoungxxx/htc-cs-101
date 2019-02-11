1eaeda5980c563c7c7468b87868b8075f3170f94
Joseph Maxwell <joseph.maxwell@hyland.com>
2019-02-11 08:33:57 -0500

    add week02 content

---
+++ Week02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create variables and do things with them!
+            
+        }
+    }
+}
