0bfd1c295a89f2d7bb81a1adafa76d112404047e
User <User@TECH-018436.OnBase.net>
2019-02-19 18:25:26 -0500

    Blah

---
+++ Student Work/Week 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What grade are you in?");
+            int studentGrade = Convert.ToInt32(Console.ReadLine());
+
+                if (studentGrade <= 5) 
+                {
+                    Console.WriteLine("You are in Elementry School");
+                }
+                else if (studentGrade <= 8)
+                {
+                    Console.WriteLine("You are in Middle School");
+                }
+                else if (studentGrade <= 12)
+                {
+                    Console.WriteLine("You are in High School");
+                }
+                else
+                {
+                    Console.WriteLine("You are in College or a free adult or you're a liar");
+                }
+
+
+        }
+    }
+}
