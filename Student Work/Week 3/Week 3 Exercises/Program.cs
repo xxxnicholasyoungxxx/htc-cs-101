687a0d53b855be9242ea2eee44a30ee72bc4e7a2
User <User@TECH-018436.OnBase.net>
2019-02-19 18:46:54 -0500

    Exercise

---
+++ Student Work/Week 3/Week 3 Exercises/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Week_3_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number between 1 and 10");
+            int num = Convert.ToInt32(Console.ReadLine());
+                if (num > 4) {
+                    Console.WriteLine("Too High");
+                }
+                else if (num < 4) {
+                    Console.WriteLine("Too Low");
+                }
+                else {
+                    Console.WriteLine("Just Right");
+                }
+        
+        }
+    }
+}
