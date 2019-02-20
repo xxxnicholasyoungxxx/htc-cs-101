6feaa1808850eaf38817c32d177dfb9c20296fe0
User <User@TECH-018436.OnBase.net>
2019-02-19 19:29:44 -0500

    Challenge maximum of 3

---
+++ Student Work/Week 3/Week 3 Challenges/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Week_3_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Type a number, please");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0) {
+                Console.WriteLine("This number is even");
+            }
+            else {
+                Console.WriteLine("This number is odd");
+            }*/
+
+            
+            
+            /*Console.WriteLine("Type a number..");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another number..");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type one last number..");
+            int numberThree = Convert.ToInt32(Console.ReadLine());
+
+            if ((numberOne > numberTwo) && (numberOne > numberThree)) {
+                Console.WriteLine(numberOne + " Is the biggest");
+            }
+            else if ((numberTwo > numberOne) && (numberTwo > numberThree)) {
+                Console.WriteLine(numberTwo + " Is the biggest");
+            }
+            else if ((numberThree > numberOne) && (numberThree > numberTwo)) {
+                Console.WriteLine(numberThree + " Is the biggest");
+            }
+            else {}*/
+
+        }
+    }
+}
