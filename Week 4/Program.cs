80178e6e9440a588561b6875966564bc5959297d
User <User@TECH-018436.OnBase.net>
2019-02-26 18:16:29 -0500

    YE

---
+++ Week 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Week_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            bool snowOnGround = true;
+            bool foundGloves = true;
+
+            if (snowOnGround == true && foundGloves == true)
+            {
+                Console.WriteLine("I wan build snwman");
+            }
+            else
+            {
+                Console.WriteLine("I stay inside");
+            }
+            */
+
+            /* 
+            bool snowOnGround = true;
+            bool feelingSick = true;
+
+            if (snowOnGround == true || feelingSick == true) 
+            {
+                Console.WriteLine("I want soup");
+            }
+            else
+            {
+                Console.WriteLine("I want to play outside");
+            }
+             */
+
+        }
+    }
+}
---
+++ Week 4/Week04 Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week04_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
