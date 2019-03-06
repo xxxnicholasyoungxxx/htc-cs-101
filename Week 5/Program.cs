4d041ac305b453e9cbf5004ad8b1b714b0de2141
User <User@TECH-018436.OnBase.net>
2019-03-05 19:31:31 -0500

    calculator

---
+++ Week 5/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Week_5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int something = 0;
+            OutArgExample(out something);
+            Console.WriteLine(something);
+
+            OutArgExample(out something);
+            Console.WriteLine(something);
+
+           /* int returnValue = 0;
+
+            Console.WriteLine("What operation to perform?");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Enter a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (operation == "+") {
+                // add the numbers
+                returnValue = AddNumbers(a, b);
+            }
+            else if (operation == "-") {
+                // minus the number
+                returnValue = MinusNumbers(a, b);
+            }
+            else if (operation == "x") {
+                // Multiply
+                returnValue = MultiplyNumbers(a, b);
+            }
+            else if (operation == "/") {
+                // division
+                returnValue = DivideNumbers(a, b);
+            }
+            else if (operation == "quadratic") {
+
+
+
+            }
+            else {
+                Console.WriteLine("Type in a math operation");
+            }
+
+            Console.WriteLine(returnValue);
+*/
+        }
+
+        public static int AddNumbers(int integer1, int integer2)
+        {
+            return integer1 + integer2;
+        }
+
+        public static int MinusNumbers(int integer1, int integer2)
+        {
+            return integer1 - integer2;
+        }
+
+        public static int MultiplyNumbers(int integer1, int integer2)
+        {
+            return integer1 * integer2;
+        }
+        public static int DivideNumbers(int integer1, int integer2)
+        {
+            return integer1 / integer2;
+        }
+
+        public static int QuadraticNum(int a, int b, int c) 
+        {
+            return 1;
+        }
+        public static void OutArgExample(out int number1, out int number2)
+        {
+            number1 = 44;
+            number2 = 5;
+        }
+
+    }
+}
