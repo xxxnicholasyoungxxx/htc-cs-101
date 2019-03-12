d3b5e8c727ab6d17b191c3cf646355f331849e3f
User <User@TECH-018436.OnBase.net>
2019-03-12 19:27:24 -0400

    Finish challenge 1

---
+++ week 6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace week_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gimme a numbah");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What number do you want to end at??");
+            int end = Convert.ToInt32(Console.ReadLine());
+
+            while (x < end) {
+                x++;
+                //Console.WriteLine(x);
+                    if (x % 2 == 0) {
+                        Console.WriteLine("This number is even");
+                        Console.WriteLine(x);
+                    } else {
+                        //Console.WriteLine("This number is odd");
+                    }
+            }
+
+            /*if (x % 2 == 0) {
+                Console.WriteLine("This number is even");
+            } else {
+                Console.WriteLine("This number is odd");
+            }*/
+
+        }
+    }
+}
---
+++ week 6/week 6 challenges/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace week_6_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool playAgain = true;
+
+            while (playAgain == true) {
+
+                
+
+                Console.WriteLine("Guess a number 1-20 that I am thinking of..");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Random rnd = new Random();
+                int sumfin = rnd.Next(20);
+
+                //Console.WriteLine(sumfin);
+
+                    while (x != sumfin) {
+
+                        if (x > sumfin) {
+                            //Too high
+                            Console.WriteLine("Too high");
+                        } else if (x < sumfin) {
+                            //Too low
+                            Console.WriteLine("Too low");
+                        }
+                        Console.WriteLine("Guess again");
+                        x = Convert.ToInt32(Console.ReadLine());
+
+                    }
+
+                Console.WriteLine("You guessed it!!");
+
+                Console.WriteLine("Wanna try again?? true or false");
+                bool play = Convert.ToBoolean(Console.ReadLine());
+
+                playAgain = play;
+
+            }
+
+        }
+    }
+}
