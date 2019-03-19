814277dc2ba59632f67cb849b5aa1c7191d6429a
User <User@TECH-018436.OnBase.net>
2019-03-19 19:30:05 -0400

    Challenges

---
+++ week7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type a number plez");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= num; i++) {
+                    if (i % 3 == 0 && i % 5 == 0) {
+                        Console.WriteLine("FIZZBUZZ");
+                    } else if (i % 3 == 0) {
+                        Console.WriteLine("FIZZ");
+                    } else if (i % 5 == 0) {
+                        Console.WriteLine("BUZZ");
+                    } else {
+                        Console.WriteLine(i);
+                    }
+                }
+        
+        
+        }
+    }
+}
---
+++ week7/week 7 challenges/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace week_7_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in an ending point..");
+            int topNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Where do you want to start counting??");
+            int botNum = Convert.ToInt32(Console.ReadLine());
+
+                int sum = 0;
+
+                        for (int i = botNum; i <= topNum; i++) {
+                            sum = i + sum;
+                        }
+                    Console.WriteLine(sum);            
+        }
+    }
+}
