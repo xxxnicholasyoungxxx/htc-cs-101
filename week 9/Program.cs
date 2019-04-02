a8e9b00fcdd00768ca94478854dfb40da9212200
User <User@TECH-018436.OnBase.net>
2019-04-02 19:29:56 -0400

    hi

---
+++ week 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace week_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many? ;))");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0) {
+                PrintSmileys(num);
+            } else {
+                Console.WriteLine("Bigger number bruh");
+            }
+        }
+
+        public static void PrintSmileys(int numTimes)
+        {
+            for (int i = 0; i < numTimes; i++) {
+                Console.WriteLine("( ͜。 ͡ʖ ͜。)[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]");
+            }
+        }
+
+    }
+}
---
+++ week 9/week 9 chal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace week_9_chal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("How many?"); // chal 1
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // PrintHat(num);
+
+
+        }
+
+        // static void PrintHat(int eh) //chal 1
+        // {
+        //     for (int i = 0; i < eh; i++) {
+        //         Console.WriteLine("   .--------.");
+        //         Console.WriteLine("  /          `.");
+        //         Console.WriteLine(" |         .-. .");
+        //         Console.WriteLine(" |         |  `.|");
+        //         Console.WriteLine(" |_________|   <|>");
+        //         Console.WriteLine("(___________)");
+        //     }
+        // }
+
+        static void CouponCode(double intPrice, string finPrice)
+        {
+            if (coupCode = "PUNCH") {
+                return Console.WriteLine("intPrice 15% discount");
+            }
+        }
+    }
+}
