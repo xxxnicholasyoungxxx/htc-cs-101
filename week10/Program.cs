a9b731a1ad798223ffbf6ed51aa27738c824da1e
User <User@TECH-018436.OnBase.net>
2019-04-09 19:30:48 -0400

    ye

---
+++ week10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace week10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to go for an adventure?");
+            string ans = Console.ReadLine();
+                if (ans == "yes" || ans == "Yes" || ans == "YES") {
+                    Console.WriteLine("Good..");
+                } else {
+                    Console.WriteLine("Too bad!");
+                }
+            Console.WriteLine("Pick a number, one through three..");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int z = 1;
+                while (z == 2) {
+                    if (num == 1) {
+                        Console.WriteLine("1");
+                        z++;
+                    } else if (num == 2) {
+                        Console.WriteLine("2");
+                        z++;
+                    } else if (num == 3) {
+                        Console.WriteLine("3");
+                        z++;
+                    } else {
+                        Console.WriteLine("Number between one through three..");
+                    }
+                }
+        }
+    }
+}
---
+++ week10/week 10 code along/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week_10_code_along
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the To-Do List");
+            List<string> toDoList = new List<string>();
+
+            while (true) {
+                Console.WriteLine("Enter Command..");
+                string command = Console.ReadLine();
+
+                    if (command == "Add") {
+                        Console.WriteLine("Enter an item: ");
+                        string item = Console.ReadLine();
+                        toDoList.Add(item);
+                    } else if (command == "View") {
+                        viewList(toDoList);
+                    } else if (command == "Exit") {
+                        break;
+                    }
+            }
+        }
+        public static void viewList(List<string> stringList) {
+            int toDoCount = stringList.Count;
+                if (toDoCount == 0) {
+                    Console.WriteLine("You have nothing to do..");
+                } else if (toDoCount == 1) {
+                    Console.WriteLine(toDoCount + " thing left to do..");
+                        Console.WriteLine("To Do: ");
+                        foreach (string item in stringList) {
+                            Console.WriteLine("- " + item);
+                        }
+                } else {
+                    Console.WriteLine(toDoCount + " things left to do..");
+                        Console.WriteLine("To Do: ");
+                    foreach (string item in stringList) {
+                        Console.WriteLine("- " + item);
+                    }
+                }
+            
+        }
+    }
+}
